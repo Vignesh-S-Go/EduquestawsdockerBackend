1caa8af07508ee5dabde051206be4f0a16754154
Vignesh S <2300033852@kluniversity.in>
2025-09-23 23:35:23 +0530

    Consolidate CrossOrigin annotations into one

--- src/main/java/com/vip/Controller.java
+++ src/main/java/com/vip/Controller.java
@@ -9,8 +9,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
-@CrossOrigin(origins = "http://ec2-13-201-34-207.ap-south-1.compute.amazonaws.com")
+@CrossOrigin(origins = { "http://localhost:5173", "http://ec2-13-201-34-207.ap-south-1.compute.amazonaws.com" })
 @RequestMapping("/api")
 public class Controller {
 
